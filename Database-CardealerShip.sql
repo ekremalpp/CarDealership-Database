4bf2b05cc0f18dd38b50f49b1a7644bc97d62298
Ekrem <ealp@pgh.yearup.org>
2024-11-22 11:54:08 -0500

    created Dealerships table

--- Database-CardealerShip.sql
+++ Database-CardealerShip.sql
@@ -4,3 +4,22 @@ DROP DATABASE IF EXISTS CarDealership;
 -- Create the CarDealership database
 CREATE DATABASE CarDealership;
 
+-- CREATE DATABASE IF NOT EXISTS CarDealership;
+USE CarDealership;
+
+-- Drop tables if they exist
+DROP TABLE IF EXISTS Dealerships;
+DROP TABLE IF EXISTS Vehicles;
+DROP TABLE IF EXISTS Inventory;
+DROP TABLE IF EXISTS Sales_contracts;
+
+-- Create the dealerships table 
+
+CREATE TABLE Dealerships (
+DealershipID INT AUTO_INCREMENT PRIMARY KEY,
+DealershipName VARCHAR(100) NOT NULL,
+DealershipAddress VARCHAR(100) NOT NULL,
+DealershipPhone VARCHAR(50) NOT NULL,
+
+
+
