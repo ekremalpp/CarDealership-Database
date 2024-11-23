204b38ece5acd643f2ef0077c19be62a7204513a
Ekrem <ealp@pgh.yearup.org>
2024-11-23 09:34:49 -0500

    created sales_contracts table

--- Database-CardealerShip.sql
+++ Database-CardealerShip.sql
@@ -16,7 +16,7 @@ DROP TABLE IF EXISTS Sales_contracts;
 -- Create the Dealerships table 
 
 CREATE TABLE Dealerships (
-DealershipID INT AUTO_INCREMENT PRIMARY KEY,
+Dealership_ID INT AUTO_INCREMENT PRIMARY KEY,
 DealershipName VARCHAR(50) NOT NULL,
 DealershipAddress VARCHAR(50) NOT NULL,
 DealershipPhone VARCHAR(12) NOT NULL
@@ -39,10 +39,21 @@ Sold BOOLEAN DEFAULT 0
 -- Create the Inventory table
 CREATE TABLE Inventory(
 dealership_id INT NOT NULL,
-VIN VARCHAR(50) NOT NULL
+VIN VARCHAR(50) NOT NULL,
+FOREIGN KEY(dealership_id) REFERENCES Dealerships(Dealership_id),
+FOREIGN KEY(VIN) REFERENCES Vehicles(VIN)
 );
 
-
+-- Create Sales_contracts table
+CREATE TABLE Sales_contracts(
+Contract_Id INT AUTO_INCREMENT PRIMARY KEY,
+Buyer_name VARCHAR(50),
+Buyer_contract  DOUBLE NOT NULL,
+VIN VARCHAR(50),
+Sales_date INT NOT NULL,
+Price DOUBLE NOT NULL,
+FOREIGN KEY(VIN) REFERENCES Vehicles(VIN)
+);
 
 
 
