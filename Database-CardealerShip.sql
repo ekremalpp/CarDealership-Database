e69d151b8e8805501883cd08f18c594a50ddc84d
Ekrem <ealp@pgh.yearup.org>
2024-11-24 10:35:12 -0500

    inserted datas into vehicles table

--- Database-CardealerShip.sql
+++ Database-CardealerShip.sql
@@ -68,6 +68,19 @@ VALUES
 ('MetroCar Sales', '333 River Ave, Austin, TX', '512-555-4455'),
 ('Elite Vehicle Sales', '555 Elite Dr, Scottsdale, AZ', '480-555-6677');
 
+INSERT INTO Vehicles (VIN, Year, Make, Model, Type, Color, Idometer, Price, Sold)
+VALUES
+('1HGCM82633A123456', 2020, 'Honda', 'Civic', 'Sedan', 'Blue', 22000, 20000.50, 0),
+('2FMDK3GC1BBA654321', 2018, 'Ford', 'Escape', 'SUV', 'Red', 45000, 18000.75, 0),
+('3VW2B7AT6BM054987', 2019, 'Volkswagen', 'Jetta', 'Sedan', 'Black', 30000, 16000.25, 1),
+('4T1BF1FK0HU123654', 2021, 'Toyota', 'Camry', 'Sedan', 'White', 15000, 24000.00, 0),
+('5NMS3CAD1LH654321', 2020, 'Hyundai', 'Santa Fe', 'SUV', 'Silver', 35000, 28000.99, 1),
+('1G1ZD5ST0LF123456', 2020, 'Chevrolet', 'Malibu', 'Sedan', 'Gray', 25000, 19000.00, 0),
+('2C4RDGBG0KR123987', 2019, 'Chrysler', 'Pacifica', 'Van', 'Blue', 40000, 27000.50, 0),
+('3GNCJNSB5JL123789', 2021, 'Chevrolet', 'Trax', 'SUV', 'Green', 15000, 21000.99, 1),
+('5LMJJ3JT2LE123890', 2020, 'Lincoln', 'Nautilus', 'SUV', 'White', 22000, 45000.00, 0),
+('1N4BL4CV4HC123456', 2018, 'Nissan', 'Altima', 'Sedan', 'Black', 55000, 13000.50, 1);
+
 
 
 
